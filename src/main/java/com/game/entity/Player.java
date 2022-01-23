d14335771707e438596e41093343e04999313668
oldmermaidman <a.v.nikolaef@gmail.com>
2022-01-24 01:05:52 +0300

    added: Player, PlayerController, PlayerService, PlayerRepository, Exception classes

---
+++ src/main/java/com/game/entity/Player.java
@@ -0,0 +1,163 @@
+package com.game.entity;
+
+import com.game.controller.PlayerOrder;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "player")
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "race")
+    @Enumerated(EnumType.STRING)
+    private Race race;
+
+    @Column(name = "profession")
+    @Enumerated(EnumType.STRING)
+    private Profession profession;
+
+    @Column(name = "level")
+    private Integer level;
+
+    @Column(name = "untilNextLevel")
+    private Integer untilNextLevel;
+
+    @Column(name = "birthday")
+    private Date birthday;
+
+    @Column(name = "banned")
+    private Boolean banned;
+
+    @Column(name = "experience")
+    private Integer experience;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Race getRace() {
+        return race;
+    }
+
+    public void setRace(Race race) {
+        this.race = race;
+    }
+
+    public Profession getProfession() {
+        return profession;
+    }
+
+    public void setProfession(Profession profession) {
+        this.profession = profession;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Integer getUntilNextLevel() {
+        return untilNextLevel;
+    }
+
+    public void setUntilNextLevel(Integer untilNextLevel) {
+        this.untilNextLevel = untilNextLevel;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Boolean getBanned() {
+        return banned;
+    }
+
+    public void setBanned(Boolean banned) {
+        this.banned = banned;
+    }
+
+    public Integer getExperience() {
+        return experience;
+    }
+
+    public void setExperience(Integer experience) {
+        this.experience = experience;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(id, player.id) &&
+                Objects.equals(name, player.name) &&
+                Objects.equals(title, player.title) &&
+                race == player.race &&
+                profession == player.profession &&
+                Objects.equals(level, player.level) &&
+                Objects.equals(untilNextLevel, player.untilNextLevel) &&
+                Objects.equals(birthday, player.birthday) &&
+                Objects.equals(banned, player.banned) &&
+                Objects.equals(experience, player.experience);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, title, race, profession, level, untilNextLevel, birthday, banned, experience);
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", title='" + title + '\'' +
+                ", race=" + race +
+                ", profession=" + profession +
+                ", level=" + level +
+                ", untilNextLevel=" + untilNextLevel +
+                ", birthday=" + birthday +
+                ", banned=" + banned +
+                ", experience=" + experience +
+                '}';
+    }
+}
